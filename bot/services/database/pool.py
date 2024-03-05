0b4dc8ddb21cf34392e5220e0d5d497ef0ffde93
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-03-06 03:01:03 +0500

    Src layout

--- bot/services/database/pool.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
-
-def create_pool(dsn: str) -> async_sessionmaker[AsyncSession]:
-    engine = create_async_engine(dsn, pool_size=10, max_overflow=10)
-    session_pool = async_sessionmaker(bind=engine, expire_on_commit=False)
-    return session_pool

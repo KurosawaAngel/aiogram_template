f5248b95f228f281f84a79f2756c26ed80e1b3b1
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-03-18 17:19:37 +0500

    resolve

--- src/aiogram_template/runners.py
+++ src/aiogram_template/runners.py
@@ -6,12 +6,13 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from .settings import Config
 
 
-async def on_startup(bot: Bot, config: Config) -> None:
+async def on_startup(bot: Bot, config: Config, dispatcher: Dispatcher) -> None:
     if config.webhook.use:
         await bot.set_webhook(
             config.webhook_url,
             drop_pending_updates=config.common.drop_pending_updates,
             secret_token=config.webhook.secret.get_secret_value(),
+            allowed_updates=dispatcher.resolve_used_update_types(),
         )
         return
     await bot.delete_webhook(drop_pending_updates=config.common.drop_pending_updates)

1ca1a9be57a29fa6055eb09c2cb05fb5cbb727d8
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-03-22 21:27:51 +0500

    added logging

--- src/aiogram_template/__main__.py
+++ src/aiogram_template/__main__.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram_template.factories.bot import create_bot
 from aiogram_template.factories.dispatcher import setup_dispatcher
 from aiogram_template.runners import run_webhook
@@ -14,4 +16,9 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(name)s - %(asctime)s | %(levelname)s | %(message)s",
+        level=logging.INFO,
+        datefmt="%d/%m/%Y %H:%M:%S",
+    )
     main()

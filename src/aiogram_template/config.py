d2296aca2168f7b5036c234568b8a09dd6823e54
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-04-13 18:50:02 +0500

    fixed mypy issues

--- src/aiogram_template/config.py
+++ src/aiogram_template/config.py
@@ -1,31 +1,30 @@
 from secrets import token_urlsafe
 from typing import Self
 
-from pydantic import Field, SecretStr
-from pydantic_settings import BaseSettings as _BaseSettings
-from pydantic_settings import SettingsConfigDict
+from pydantic import BaseModel, Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class BaseConfig(_BaseSettings):
+class BaseConfig(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore", frozen=True
     )
 
 
 class CommonConfig(BaseConfig, env_prefix="COMMON_"):
-    token: SecretStr
-    admin_chat_id: int
-    drop_pending_updates: bool
+    token: SecretStr = Field(default="")
+    admin_chat_id: int = -1
+    drop_pending_updates: bool = True
 
 
 class WebhookConfig(BaseConfig, env_prefix="WEBHOOK_"):
-    host: str
-    base: str
-    path: str
-    port: int
-    secret: SecretStr = Field(default=token_urlsafe)
-    reset: bool
-    use: bool
+    host: str = "localhost"
+    base: str = ""
+    path: str = "/webhook"
+    port: int = 80
+    secret: SecretStr = Field(default_factory=token_urlsafe)
+    reset: bool = True
+    use: bool = False
 
     @property
     def url(self) -> str:
@@ -34,11 +33,11 @@ class WebhookConfig(BaseConfig, env_prefix="WEBHOOK_"):
 
 
 class PostgresConfig(BaseConfig, env_prefix="POSTGRES_"):
-    host: str
-    db: str
-    password: str
-    port: int
-    user: str
+    host: str = "localhost"
+    db: str = "postgres"
+    password: str = "postgres"
+    port: int = 5432
+    user: str = "postgres"
 
     @property
     def url(self) -> str:
@@ -46,16 +45,16 @@ class PostgresConfig(BaseConfig, env_prefix="POSTGRES_"):
 
 
 class RedisConfig(BaseConfig, env_prefix="REDIS_"):
-    host: str
-    port: int
-    database: int
+    host: str = "localhost"
+    port: int = 6379
+    database: int = 0
 
     @property
     def url(self) -> str:
         return f"redis://{self.host}:{self.port}/{self.database}"
 
 
-class Config(BaseConfig):
+class Config(BaseModel):
     common: CommonConfig
     webhook: WebhookConfig
     postgres: PostgresConfig

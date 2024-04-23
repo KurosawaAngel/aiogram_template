6c47639bf73e15ed4dddecc4a88a39950c90eb7f
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-04-23 13:07:40 +0500

    mini upd

--- src/aiogram_template/services/database/repositories/base.py
+++ src/aiogram_template/services/database/repositories/base.py
@@ -1,7 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from aiogram_template.services.database.models import BaseModel
-
 
 class BaseRepository:
     _session: AsyncSession
@@ -13,6 +11,3 @@ class BaseRepository:
 
     async def commit(self) -> None:
         await self._session.commit()
-
-    def add(self, *entity: BaseModel) -> None:
-        self._session.add_all(entity)

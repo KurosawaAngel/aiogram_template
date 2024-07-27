1051a55c81ff8dbbf9eb2817cf2d005de0010e1a
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-07-27 22:12:05 +0500

    mini

--- src/aiogram_template/di/gateway.py
+++ src/aiogram_template/di/gateway.py
@@ -1,5 +1,4 @@
 from dishka import Provider, Scope, provide
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from aiogram_template.data.database import gateways as gate
 
@@ -7,6 +6,4 @@ from aiogram_template.data.database import gateways as gate
 class GatewayProvider(Provider):
     scope = Scope.REQUEST
 
-    @provide
-    def get_user_gateway(self, session: AsyncSession) -> gate.UserGateway:
-        return gate.UserGateway(session)
+    user_gateway = provide(gate.UserGateway)

a6442cf188c2a0bec1e55550b05901e5a4f8088b
Kurosawa <145038102+KurosawaAngel@users.noreply.github.com>
2024-10-02 02:07:10 +0300

    fix

--- src/aiogram_template/runner.py
+++ src/aiogram_template/runner.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 from aiogram_dialog.api.entities import DIALOG_EVENT_NAME
 from aiohttp import web
 from dishka import AsyncContainer, FromDishka
-from dishka.integrations.aiohttp import setup_dishka
+from dishka.integrations.aiohttp import inject, setup_dishka
 
 from aiogram_template import handlers
 from aiogram_template.config import WebhookConfig
@@ -13,10 +13,10 @@ from aiogram_template.config import WebhookConfig
 
 def run_webhook(config: WebhookConfig, container: AsyncContainer) -> None:
     app = web.Application()
-    app.add_routes((handlers.bot_router,))
+    setup_dishka(container, app, auto_inject=True)
+    app.add_routes(handlers.bot_router)
     app.on_startup.append(_on_startup)
     app.on_shutdown.append(_on_shutdown)
-    setup_dishka(container, app, auto_inject=True)
 
     return web.run_app(app, host=config.host, port=config.port)
 
@@ -41,6 +41,7 @@ async def start_polling(container: AsyncContainer) -> None:
         await container.close()
 
 
+@inject
 async def _on_startup(
     app: web.Application, dp: FromDishka[Dispatcher], bot: FromDishka[Bot]
 ) -> None:
@@ -53,6 +54,7 @@ async def _on_startup(
     await dp.emit_startup(**workflow_data)
 
 
+@inject
 async def _on_shutdown(
     app: web.Application, dp: FromDishka[Dispatcher], bot: FromDishka[Bot]
 ) -> None:
